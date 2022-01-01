cbaf5796386b981114574de312f9e02e2546fd96
chosangho <hyundai_sangho@naver.com>
2022-01-01 13:31:42 +0900

    index.php, search.php 생성

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Welcome to Doodle</title>
+</head>
+
+<body>
+  <div class="mainSection">
+    <div class="logoContainer">
+      <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
+    </div>
+
+    <div class="searchContainer">
+      <form action="search.php" method="GET">
+        <input class="searchBox" type="text" name="term">
+        <input class="searchButton" type="submit" value="Search">
+      </form>
+    </div>
+
+  </div>
+</body>
+
+</html>
