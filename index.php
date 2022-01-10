87f4cd91bf6e2be4e78d45bc03a48d7299c6b38f
chosangho <hyundai_sangho@naver.com>
2022-01-10 22:13:10 +0900

    style.css 수정, choggle.png 추가

--- index.php
+++ index.php
@@ -5,20 +5,25 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/style.css">
   <title>Welcome to Doodle</title>
 </head>
 
 <body>
-  <div class="mainSection">
-    <div class="logoContainer">
-      <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
-    </div>
+  <div class="wrapper indexPage">
 
-    <div class="searchContainer">
-      <form action="search.php" method="GET">
-        <input class="searchBox" type="text" name="term">
-        <input class="searchButton" type="submit" value="Search">
-      </form>
+    <div class="mainSection">
+
+      <div class="logoContainer">
+        <img src="assets/images/chogle.png">
+      </div>
+
+      <div class="searchContainer">
+        <form action="search.php" method="GET">
+          <input class="searchBox" type="text" name="term">
+          <input class="searchButton" type="submit" value="Search">
+        </form>
+      </div>
     </div>
 
   </div>
